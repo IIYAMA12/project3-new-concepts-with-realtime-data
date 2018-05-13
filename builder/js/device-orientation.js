57f0fed0c10e5340ba196d0dad76010c699d17fb
IIYAMA12 <jorik@MBP-van-jw.lan>
2018-05-14 00:26:48 +0200

    large update

--- builder/js/device-orientation.js
+++ builder/js/device-orientation.js
@@ -11,18 +11,45 @@
         // console.log(absolute, alpha, beta, gamma);
         // document.getElementsByTagName("textarea")[0].value = alpha;
         // Do stuff with the new orientation data
-        const threshold = 5;
+        const threshold = 14;
         const range = 60;
     
         // mainRotation = gamma
         document.getElementById("main-background").style.transform = "rotate(" + -gamma + "deg)";
 
-        if (gamma > 0) {
-            document.getElementById("main-vis").style.transform = "rotate(" + -gamma + "deg) translateY(40vh)";
-        } else {
-            document.getElementById("main-vis").style.transform = "rotate(" + -gamma + "deg) translateY(40vh) scaleX(-1)";
+        const deviceOrientationSVG = document.getElementById("device-orientation-indicator").getElementsByTagName("svg")[0];
+
+        deviceOrientationSVG.style.transform = "translate(calc(" + (gamma + 90 / 180 * 100) + "vw - 1.5rem)";
+
+        
+
+        const rotateWithScreenElements = document.querySelectorAll(".rotate-with-screen .rotate-container");
+
+        for (let index = 0; index < rotateWithScreenElements.length; index++) {
+            const element = rotateWithScreenElements[index];
+            // let transfromString = element.style.transform;
+            
+            // const transformStringParts = transfromString.split(" ");
+            // transfromString = "";
+
+            // for (let i = 0; i < transformStringParts.length; i++) {
+            //     const string = transformStringParts[i];
+            //     console.log("??", string)
+            //     if ((string[0] != "r" && string[1] != "o") && (string[0] != "s" && string[1] != "c")) {
+            //         transfromString += string;
+                    
+            //     }   
+            // }
+            
+
+            if (gamma > 0) {
+                element.style.transform =  "rotate(" + -gamma + "deg)";
+            } else {
+                element.style.transform = "rotate(" + -gamma + "deg) scaleX(-1)";
+            }
+            element.classList.toggle("flip-h", gamma < 0);
         }
-        document.getElementById("main-vis").classList.toggle("flip-h", gamma < 0);
+
 
 
 
@@ -40,21 +67,29 @@
         
 
         cameraDirection = "stop"; 
+        // deviceOrientationSVG.getElementsByTagName("g")[0].setAttribute("fill", "gray");
         if (beta > 5 && beta < 135) {
             if (gamma > -range && gamma < range) {
                 // document.getElementsByTagName("textarea")[0].value = alpha + "?";
                 if (gamma < -threshold) {
                     // document.getElementsByTagName("textarea")[0].value += "yes1";
                     cameraDirection = "left";
+                    // deviceOrientationSVG.getElementsByTagName("g")[0].setAttribute("fill", "white");
+                    deviceOrientationSVG.getElementsByTagName("g")[0].setAttribute("direction", "left");
+
                     // document.getElementsByTagName("textarea")[0].value = gamma + "left";
+                    return true;
                 } else if (gamma > threshold) {
                     // document.getElementsByTagName("textarea")[0].value += "yes2";
                     cameraDirection = "right";
+                    // deviceOrientationSVG.getElementsByTagName("g")[0].setAttribute("fill", "white");
+                    deviceOrientationSVG.getElementsByTagName("g")[0].setAttribute("direction", "right");
                     // document.getElementsByTagName("textarea")[0].value = gamma + "right";
+                    return true;
                 }
             }
         }
-        debug(cameraDirection);
+        deviceOrientationSVG.getElementsByTagName("g")[0].setAttribute("direction", "stop");
     }    
     window.addEventListener("deviceorientation", handleOrientation, true);
 })();
--- public/js/camera-control-debug.js
+++ public/js/camera-control-debug.js
@@ -1,26 +1,199 @@
 
 let cameraDirection = "stop";
 
-(function () {
-    let slidePosition = 0;
-    
+const slideControl = (function () {
+
     const slides = [
         {
-            length: 30 
+            length: 4,
+            elementId: "intro",
+            functions: {
+                start: [
+                    function () {
+                        document.getElementById("main-crop").classList.add("on-screen");
+                        document.getElementById("main-fish").classList.add("on-plate");
+                        document.getElementById("main-crop").classList.add("on-plate");
+                        
+                    }
+                ],
+                end: [
+                    function () {
+                        document.getElementById("main-fish").classList.remove("on-plate");
+                        document.getElementById("main-crop").classList.remove("on-plate");
+                        document.getElementById("main-crop").classList.remove("on-screen");
+                    }
+                ]
+            }
         },
         {
-            length: 5
+            length: 4,
+            functions: {
+                start: [
+                    function () {
+                        
+                        
+                        document.getElementById("main-fish").classList.add("on-bottom-section");
+                        document.getElementById("main-crop").classList.add("on-bottom-section");
+
+                        document.getElementById("main-crop").classList.add("on-screen");
+                        document.getElementById("waves").classList.add("on-screen");
+                    }
+                ],
+                end: [
+                    function () {
+                        
+                        document.getElementById("main-fish").classList.remove("on-bottom-section");
+                        document.getElementById("main-crop").classList.remove("on-bottom-section");
+
+                        document.getElementById("main-crop").classList.remove("on-screen");
+                        document.getElementById("waves").classList.remove("on-screen");
+                    }
+                ]
+            },
+            elementId: "show-plate-content",
         },
         {
-            length: 30 
+            length: 2,
+            elementId: "",
+        },
+        {
+            length: 4,
+            elementId: "toilet",
+            functions: {
+                start: [
+                    function () {
+                        document.getElementById("main-fish").classList.add("shitting");
+                        document.getElementById("main-fish").classList.add("on-screen");
+                    }
+                ],
+                end: [
+                    function () {
+                        
+                        document.getElementById("main-fish").classList.remove("shitting");
+
+                    }
+                ]
+            },
+        },
+        {
+            length: 4,
+            elementId: "nitrosomonas",
+            functions: {
+                start: [
+                    function () {
+                        document.getElementById("main-fish").classList.remove("on-screen");
+                    }
+                ]
+            }
+        },
+        {
+            length: 4,
+            elementId: "nitrospira",
+        },
+        {
+            length: 4,
+            elementId: "plants",
+        },
+        {
+            length: 4,
+            elementId: "clean-water",
+        },
+        {
+            length: 4,
+            elementId: "fish-tank",
+        },
+        {
+            length: 1,
+            elementId: "break",
+            functions: {
+                start: [
+                    function () {
+                        document.getElementById("main-fish").classList.remove("on-screen");
+                    }
+                ]
+            }
         },
     ];
 
-    const totalSlideSize = slides.reduce(function (accumulator, currentValue) {
-        return accumulator + currentValue.length;
-    }, 0);
-    // console.table("totalSlideSize", totalSlideSize);
+   
+
+    const slideControl = {
+        check: function () {
+            const slides = this.slides;
+            const progress = this.slidePosition / this.totalSize;
+            let slideOffset = 0;
+            for (let i = 0; i < slides.length; i++) {
+                const slide = slides[i];
+                if (this.slidePosition <= slideOffset + slide.length) {
+                    console.log("slide index", i, this.slidePosition);
+                    slideControl.show(i);
+                    break;
+                } else {
+                    slideOffset += slide.length;
+                }
+            }
+            
+        },
+        show: function (index) {
+            const slide = this.slides[index];
+            let newElementId;
+            if (slide != undefined) {
+                newElementId = slide.elementId;
+            }
+
+
+            const previousSlide = this.slideShown;
+            const previousElementId = previousSlide.elementId;
+            if (newElementId !== previousElementId) {
+
+                
+                if (previousSlide.functions != undefined && previousSlide.functions.end != undefined) {
+                    const endFunctions = previousSlide.functions.end;
+                    for (let i = 0; i < endFunctions.length; i++) {
+                        endFunctions[i]();
+                    }
+                }
+                
+                if (slide.functions != undefined && slide.functions.start != undefined) {
+                    const startFunctions = slide.functions.start;
+                    for (let i = 0; i < startFunctions.length; i++) {
+                        startFunctions[i]();
+                    }
+                }
+
+                if (previousElementId != undefined) {
+                    const previousElement = document.getElementById(previousElementId);
+                    if (previousElement != undefined) {
+                        previousElement.classList.add("hide-article");
+                    }
+                }
+                if (newElementId != undefined) {
+                    const elementElement = document.getElementById(newElementId);
+                    if (elementElement != undefined) {
+                        elementElement.classList.remove("hide-article");
+                        elementElement.classList.add("active");
+                    }
+                }
+            }
+            this.slideShown = slide;
+        },
+        slideShown: slides[0],
+        slides: slides,
+        totalSize: slides.reduce(function (accumulator, currentValue) {
+            return accumulator + currentValue.length;
+        }, 0),
+        slidePosition: slides[0].length / 2
+    };
+
+    return slideControl;
+})();
+
+
+(function () {
+    let slidePosition = slideControl.slidePosition;
     
+    
+    const totalSlideSize = slideControl.totalSize;
 
     window.addEventListener("load", function () {
         let lastTimeStamp;
@@ -33,16 +206,17 @@ let cameraDirection = "stop";
 
                 switch (cameraDirection) {
                     case "left":
-                        slidePosition += speedFactor * cameraSpeed;
-                        if (slidePosition > totalSlideSize) {
-                            slidePosition = 0;
-                        } 
-                        break;
-                    case "right":
                         slidePosition -= speedFactor * cameraSpeed;
                         if (slidePosition < 0) {
                             slidePosition = totalSlideSize;
                         }
+                        break;
+                    case "right":
+
+                        slidePosition += speedFactor * cameraSpeed;
+                        if (slidePosition > totalSlideSize) {
+                            slidePosition = 0;
+                        } 
                         break;
                     default:
 
@@ -61,14 +235,19 @@ let cameraDirection = "stop";
                     }, 50);
                 }
                 lastBackgroundProgress = progress;
-                console.log(document.getElementsByTagName("main")[0].style.backgroundPosition);
+
+                slideControl.check();
+                
                 const backgroundProgress = slidePosition % 10 / 10;
-                const backgroundPositionX = (backgroundProgress * 200) - 50;
+                const backgroundPositionX = (200 - (backgroundProgress * 200)) - 50;
                 document.getElementById("main-background").style.backgroundPosition = "" + backgroundPositionX + "vw calc(150vmax - 50vh)";
-                // document.getElementsByTagName("textarea")[0].value = progress;
             }
+
+            slideControl.slidePosition = slidePosition;
             lastTimeStamp = timeStamp;
         });
     });    
 })();
+
+
 
